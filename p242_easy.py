670589960ccab54c09d11d07843c5394841eddcb
ilay <ilay.raz@gmail.com>
2015-12-30 02:19:51 -0800

    p242_easy

---
+++ p242_easy.py
@@ -0,0 +1,26 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/3twuwf/20151123_challenge_242_easy_funny_plant/
+# Problem follows a bisection of Fibonacci seqence, A001906
+
+
+def fruitGen():
+    """ output: (week, fruit)"""
+    i, a, b = 2, 1, 2
+
+    yield (1, 0)
+
+    while True:
+        yield (i, a)
+        a += b
+        b += a
+        i += 1
+
+
+def plant(x, y):
+    """
+        Finds how many weeks it needs to support x people
+        input: (# of people, starting fruits)
+    """
+
+    for i, k in fruitGen():
+        if k * y >= x:
+            return i
